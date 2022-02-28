102b97703e680c8175401b67c1738651430d2a58
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-02-28 20:31:37 +0530

    Added basic styles working on logic

---
+++ app.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author, totalPages, readStatus) {
+	title = this.title;
+	author = this.author;
+	totalPages = this.totalPages;
+	readStatus = this.readStatus;
+}
+
+function addBookToLibrary() {
+	// do stuff here
+}
