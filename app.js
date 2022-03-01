4ceabb4a2c2e165c2451a7613ac0092d6d0be069
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-03-01 21:25:16 +0530

    Hard coded basic logic, fixed some styles and added some html templates

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ function Book(id, title, author, totalPages, readStatus) {
 	this.totalPages = totalPages;
 	this.readStatus = readStatus;
 }
+Book.prototype.toggleReadStatus = function () {
+	return (this.readStatus = !this.readStatus);
+};
 
 function addBookToLibrary() {
 	return 0;
@@ -47,39 +50,66 @@ submit.addEventListener("click", () => {
 		);
 		myLibrary.push(book);
 
-		let outCard = document.createElement("div");
-		outCard.className = "book-card";
-		let img = document.createElement("img");
-		img.src = "./images/trash-can.svg";
-		let h2 = document.createElement("h2");
-		h2.innerText = book.title;
-		let strong1 = document.createElement("strong");
-		strong1.innerText = "Author: " + book.author;
-		let strong2 = document.createElement("strong");
-		strong2.innerText = "Total Pages: " + book.totalPages;
-		let span = document.createElement("span");
-		span.className = "toggle";
-		let toggleInput = document.createElement("input");
-		toggleInput.type = "checkbox";
-		toggleInput.id = `${book.id}`;
-		book.readStatus
-			? (toggleInput.checked = true)
-			: (toggleInput.checked = false);
-		let toggleLabel = document.createElement("label");
-		toggleLabel.htmlFor = `${book.id}`;
-		let para = document.createElement("p");
+		myLibrary.map((book) => {
+			if (book.id === myLibrary.length - 1) {
+				let outCard = document.createElement("div");
+				outCard.className = "book-card";
+				outCard.id = `card-${book.id}`;
 
-		span.appendChild(toggleInput);
-		span.appendChild(toggleLabel);
-		span.appendChild(para);
+				let img = document.createElement("img");
+				img.src = "./images/trash-can.svg";
+				img.className = "delete";
+				img.id = `delete-${book.id}`;
 
-		outCard.appendChild(img);
-		outCard.appendChild(h2);
-		outCard.appendChild(strong1);
-		outCard.appendChild(strong2);
-		outCard.appendChild(span);
+				let h2 = document.createElement("h2");
+				h2.innerText = book.title;
 
-		bookHolder.appendChild(outCard);
+				let strong1 = document.createElement("strong");
+				strong1.innerText = "Author: " + book.author;
+
+				let strong2 = document.createElement("strong");
+				strong2.innerText = "Total Pages: " + book.totalPages;
+
+				let span = document.createElement("span");
+				span.className = "toggle";
+
+				let toggleInput = document.createElement("input");
+				toggleInput.type = "checkbox";
+				toggleInput.id = `${book.id}`;
+				book.readStatus
+					? (toggleInput.checked = true)
+					: (toggleInput.checked = false);
+
+				let toggleLabel = document.createElement("label");
+				toggleLabel.htmlFor = `${book.id}`;
+
+				let para = document.createElement("p");
+
+				span.appendChild(toggleInput);
+				span.appendChild(toggleLabel);
+				span.appendChild(para);
+
+				outCard.appendChild(img);
+				outCard.appendChild(h2);
+				outCard.appendChild(strong1);
+				outCard.appendChild(strong2);
+				outCard.appendChild(span);
+
+				bookHolder.appendChild(outCard);
+
+				document.getElementById(book.id).addEventListener("click", () => {
+					book.toggleReadStatus();
+				});
+
+				document
+					.getElementById("delete-" + book.id)
+					.addEventListener("click", () => {
+						document.getElementById("card-" + book.id).style.display = "none";
+						myLibrary = myLibrary.filter((obj) => obj.id !== book.id);
+					});
+			}
+		});
+
 		document.querySelector(".overlay").style.cssText = "visibility: hidden";
 	}
 });
